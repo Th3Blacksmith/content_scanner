cca681d183c3faa216279fd0e2df7d41c7ba7910
SebastianBenson <58862827+SebastianBenson@users.noreply.github.com>
2020-12-28 08:59:09 +1100

    updated code for GUI

--- content_scanner.py
+++ content_scanner.py
@@ -1,40 +1,66 @@
 #!/usr/bin/env python3
 
+import PySimpleGUI as sg
 from bs4 import BeautifulSoup
-import csv
 import requests
 import re
+import pyautogui
+import webbrowser
+import time
+import os
+import sys
 
-#This script uses the words in the keywords.txt file to iteratively scan through the websites found in the file.csv file.
+cwd = os.getcwd()
+folder = 'screenshots'
+os.mkdir(folder)
 
-#First, the keywords.txt file is read and the keywords are extracted from it.
+sg.theme('Light Blue 2')
 
-keywords = []
-with open('keywords.txt') as y:
-    keywords = [line.split()[0] for line in y]
-y.close()
-keywords.pop(0)
+layout = [[sg.Text('Choose files for your wordlist and URL list')],
+          [sg.Text('Keywords', size=(8, 1)), sg.Input(), sg.FileBrowse()],
+          [sg.Text('URLs', size=(8, 1)), sg.Input(), sg.FileBrowse()],
+          [sg.Submit(), sg.Cancel()]]
 
-#Then we do the same thing for the URL containing .csv file.
+window = sg.Window('Content Scanner', layout)
 
-with open('file.csv') as f:
-    csvs = [line.split()[0] for line in f]
-csvs.pop(0)
+event, values = window.read()
+window.close()
 
-#Lastly, we actually do the work of searcing the html source code from each of our websites to see if our keyword is present.
+print(f'You clicked {event}')
+print(f'You chose filenames {values[0]} and {values[1]}')
 
-print('=================Process Starting=================')
+keywords = values[0]
+
+wordlist = [line.strip() for line in open(keywords)]
+wordlist.pop(0)
+
+websites = values[1]
+
+with open(websites) as f:
+    URLs = [line.split()[0] for line in f]
+    URLs.pop(0)
+
+print(wordlist)
+print(URLs)
+
+print('<================= Process Starting =================>')
 print('')
 print('')
-for i in keywords:
-    for url in csvs:
+for i in wordlist:
+    for url in URLs:
         page = requests.get(url)
         html = page.content
         soup = BeautifulSoup(html, 'html.parser')
-        words = soup.body.find_all(string=re.compile(i))
-        if len(words) > 0:
+        word = soup.body.find_all(string=re.compile(i))
+        if len(word) > 0:
             print('Found matching result of',i,'on '+url)
+            webbrowser.open(url, 2)
+            time.sleep(5)
+            f = open(cwd + '/screenshots/%s.png' %(i), 'w+')
+            Screenshot = pyautogui.screenshot()
+            Screenshot.save(cwd + '/screenshots/%s.png' %(i))
+            print('Taking screenshot and saving to screenshot folder...')
             print('')
 print('')
 print('')
-print('=================Process Completed=================')
+print('<================= Process Completed =================>')

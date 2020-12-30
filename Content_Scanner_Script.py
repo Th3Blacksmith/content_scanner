8dc7dbaaeaaa2ed12ed3ed90cff2e4359e16b93a
SebastianBenson <58862827+SebastianBenson@users.noreply.github.com>
2020-12-31 08:36:34 +1100

    Added banner

--- Content_Scanner_Script.py
+++ Content_Scanner_Script.py
@@ -6,6 +6,17 @@ import webbrowser
 import time
 import os
 import sys
+from colorama import init
+init(strip=not sys.stdout.isatty())
+from termcolor import cprint
+from pyfiglet import figlet_format
+
+def display_title_bar():
+    os.system('clear')
+    cprint(figlet_format('Content  Scanner'))
+
+display_title_bar()
+time.sleep(2)
 
 cwd = os.getcwd()
 folder = 'screenshots'
@@ -23,9 +34,12 @@ websites = 'URLs.csv'
 with open(websites, encoding='utf-8-sig') as f:
     URLs = [line.split()[0] for line in f]
 
+print('<====================================================>')
 print('<================= Process Starting =================>')
+print('<====================================================>')
 print('')
 print('')
+time.sleep(2)
 for i in wordlist:
     for url in URLs:
         page = requests.get(url)
@@ -34,6 +48,7 @@ for i in wordlist:
         word = soup.body.find_all(string=re.compile(i))
         if len(word) > 0:
             print('Found matching result of',i,'on '+url)
+            print('')
             webbrowser.open(url, 2)
             time.sleep(5)
             f = open(cwd + '/screenshots/%s.png' %(i), 'w+')
@@ -41,6 +56,12 @@ for i in wordlist:
             Screenshot.save(cwd + '/screenshots/%s.png' %(i))
             print('Taking screenshot and saving to screenshot folder...')
             print('')
+            time.sleep(2)
 print('')
 print('')
+print('<=====================================================>')
 print('<================= Process Completed =================>')
+print('<=====================================================>')
+time.sleep(3)
+display_title_bar()
+print('\n <================= Thankyou for using Content Scanner =================>')
